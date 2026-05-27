b4a6a84348f26b1988cd56c36085e74f155db71e
arthurwu0298 <arthurwu0298@gmail.com>
2026-05-27 14:36:32 +0800

    Add files via upload

---
+++ StockTool/api/yahoo.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  try {
+    const symbol = req.query.symbol || "2330.TW";
+
+    const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=financialData,summaryDetail,defaultKeyStatistics`;
+
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0"
+      }
+    });
+
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+}
